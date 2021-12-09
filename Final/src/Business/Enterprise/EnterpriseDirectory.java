fcec2455ef1bbb4cf70f1efea6ba94bedf99f39b
milin <milin@DESKTOP-TDFU8NF>
2021-12-09 04:01:14 -0500

    Enterprise directory half part

---
+++ Final/src/Business/Enterprise/EnterpriseDirectory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+
+import Business.Network.Network;   //Will require network to keep track
+import java.util.ArrayList;
+
+/**
+ *
+ * @author MyPC1
+ */
+public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
+   
+
+    public ArrayList<Enterprise> getEnterpriseList() {
+        return enterpriseList;
+    }
+
+    public void setEnterpriseList(ArrayList<Enterprise> enterpriseList) {
+        this.enterpriseList = enterpriseList;
+    }
+    
+    public EnterpriseDirectory(){
+        enterpriseList=new ArrayList<Enterprise>();
+    }
+    
+    //Create enterprise
+    
+}
