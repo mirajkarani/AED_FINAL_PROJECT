5d3396d8dd00447cf96b1c8edc080b8ba399319c
Deshmukh_Mandar_002194579 <deshmukh.ma@northeastern.edu>
2021-12-09 03:32:15 -0500

    Changes done in Person

--- Final/src/Business/Person/Person.java
+++ Final/src/Business/Person/Person.java
@@ -19,7 +19,19 @@ public class Person {
     String imgDetails;
     String identificationMark;
     String gender;
+    private String amt;
     private static int count = 1;
+    private boolean isSpecialChild;
+    Date registrationDate;
+    String medicalStatus;
+    private double bodytemp;
+    private double BP;
+    private double respirationRate;
+    private boolean medicalHelp;
+    private double pulseRate;
+    private boolean financialHelp;
+    
+    
     
     public String getName() {
         return name;
@@ -68,4 +80,110 @@ public class Person {
     public void setImageDetails(String imageDetails) {
         this.imgDetails = imageDetails;
     }
+    
+    public double getBP() {
+        return BP;
+    }
+
+    public void setBP(double BP) {
+        this.BP = BP;
+    }
+
+    public double getPulseRate() {
+        return pulseRate;
+    }
+
+    public void setPulseRate(double pulseRate) {
+        this.pulseRate = pulseRate;
+    }
+
+    public double getRespirationRate() {
+        return respirationRate;
+    }
+
+    public void setRespirationRate(double respirationRate) {
+        this.respirationRate = respirationRate;
+    }
+
+    public double getBodytemp() {
+        return bodytemp;
+    }
+
+    public void setBodytemp(double bodytemp) {
+        this.bodytemp = bodytemp;
+    }
+
+    public String getMedicalStatus() {
+        return medicalStatus;
+    }
+
+    public void setMedicalStatus(String medicalStatus) {
+        this.medicalStatus = medicalStatus;
+    }
+
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Person.count = count;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+
+    public int getPersonId() {
+        return PersonId;
+    }
+
+    public void setChildId(int PersonId) {
+        this.PersonId = PersonId;
+    }
+
+    
+    public boolean isFinancialHelp() {
+        return financialHelp;
+    }
+
+    public void setFinancialHelp(boolean financialHelp) {
+        this.financialHelp = financialHelp;
+    }
+
+    public String getAmt() {
+        return amt;
+    }
+
+    public void setAmt(String amt) {
+        this.amt = amt;
+    }
+
+    public boolean isMedicalHelp() {
+        return medicalHelp;
+    }
+
+    public void setMedicalHelp(boolean medicalHelp) {
+        this.medicalHelp = medicalHelp;
+    }
+
+    public boolean isIsSpecialChild() {
+        return isSpecialChild;
+    }
+
+    public void setIsSpecialChild(boolean isSpecialChild) {
+        this.isSpecialChild = isSpecialChild;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(PersonId);
+    }
 }
+
+
