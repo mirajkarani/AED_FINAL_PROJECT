d1c43a8d5c982a9416ca60da2bd112c13c406f99
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-09 18:34:04 -0500

    Added code for Donor Organization

--- Final/src/Business/Organization/DonorOrganization.java
+++ Final/src/Business/Organization/DonorOrganization.java
@@ -4,10 +4,30 @@
  */
 package Business.Organization;
 
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author aniketmirajkar
  */
-public class DonorOrganization {
+public class DonorOrganization extends Organization {
     
+    String name;
+    public DonorOrganization(String name) {
+        super(name);
+        this.name = name;
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new DonorRole());
+        return roles;
+    }
+    
+     @Override
+    public Type getType() {
+        return Organization.Type.Donor;
+    }
 }
