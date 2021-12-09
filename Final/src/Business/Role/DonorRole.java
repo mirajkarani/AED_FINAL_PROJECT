eabe633918d8eed0caea70a31cf744c2e8f5d3f9
Deshmukh_Mandar_002194579 <deshmukh.ma@northeastern.edu>
2021-12-09 18:27:54 -0500

    Additional changes to Donor role

---
+++ Final/src/Business/Role/DonorRole.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author mandardeshmukh
+ */
+public class DonorRole extends Role {
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, PersonDirectory Persondirectory, AdopterDirectory adopterdirectory,DonorDirectory donorDirectory) {
+        return new SponsorWorkArea(userProcessContainer, account, organization,enterprise, business,adopterdirectory,Persondirectory,donorDirectory);
+    }
+    
+}
