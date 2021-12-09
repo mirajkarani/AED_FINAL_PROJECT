e15a3ac0e1a1de3cb70bb026a4b6480cc61ee14b
milin <milin@DESKTOP-TDFU8NF>
2021-12-09 12:40:47 -0500

    Work Queue for adopter

---
+++ Final/src/Business/WorkQueue/AdopterRegistrationRequest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+import Business.Enterprise.Enterprise;
+
+import Business.Network.Network;
+import Business.Organization.Organization;
+
+/**
+ *
+ * @author milin
+ */
+public class AdopterRegistrationRequest extends WorkRequest{
+    
+    private String userPassword;
+    private String userEmailId;
+    private String userCity;
+    private String userContact;
+    private String ssn;
+    private String annualIncome;
+    private String gender;
+    private String address1;
+    private String address2;
+    private String age;
+    private Network network;
+    private Enterprise enterprise;
+    private Organization.Type orgType;
+    private String name;
+    private String orgName;
+    private String userName;
+  
+
+ 
+    
+    
+}
