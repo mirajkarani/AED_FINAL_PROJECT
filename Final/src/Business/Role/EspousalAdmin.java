ea4ac2507a8b14fb1a2e1e3b46ad2ed4ab36827e
Deshmukh_Mandar_002194579 <deshmukh.ma@northeastern.edu>
2021-12-09 04:35:28 -0500

    Added EspousalAdminRole

---
+++ Final/src/Business/Role/EspousalAdmin.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author mandardeshmukh
+ */
+public class EspousalAdmin {
+    
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, PersonDirectory Persondirectory, AdopterDirectory adopterdirectory,DonorDirectory donorDirectory) {
+        return new AdminWorkAreaJPanel(userProcessContainer, account, organization, enterprise, business, Persondirectory, adopterdirectory,donorDirectory);
+    }
+    
+    @Override
+    public String toString(){
+        return (Role.RoleType.EspousalAdmin.getValue());
+    }
+}
