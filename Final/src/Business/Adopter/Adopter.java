d033301e62572a3c77792884f697341cc08014f0
milin <milin@DESKTOP-TDFU8NF>
2021-12-09 03:14:34 -0500

    Adopter class definition

---
+++ Final/src/Business/Adopter/Adopter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Adopter;
+
+/**
+ *
+ * @author milin
+ */
+public class Adopter {
+
+    private int age;
+    private int userId;
+    private String username;
+    private boolean flag = false;
+    private float annualIncome;
+    private String name;
+    private String ssn;
+    private String gender;
+    private String emailId;
+
+}
