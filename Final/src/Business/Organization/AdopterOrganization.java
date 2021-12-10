42a91a9ce60e59d9703fa1b55306759f84a44f4c
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-09 22:09:43 -0500

    Completed Organization package initial code

--- Final/src/Business/Organization/AdopterOrganization.java
+++ Final/src/Business/Organization/AdopterOrganization.java
@@ -4,10 +4,31 @@
  */
 package Business.Organization;
 
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author aniketmirajkar
  */
-public class AdopterOrganization {
+public class AdopterOrganization extends Organization {
+    
+    String name;
+    
+    public AdopterOrganization(String name) {
+        super(name);
+        this.name=name;
+    
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new AdopterRole());
+        return roles;
+    }
+    @Override
+    public Organization.Type getType() {
+        return  Organization.Type.Adopter;
+    }
     
 }
