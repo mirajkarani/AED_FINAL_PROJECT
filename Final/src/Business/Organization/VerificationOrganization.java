a4cb3c1b82de7ca28f61404b5d3a08b464fddf8c
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-09 22:04:59 -0500

    Changes for verification organization

--- Final/src/Business/Organization/VerificationOrganization.java
+++ Final/src/Business/Organization/VerificationOrganization.java
@@ -4,10 +4,31 @@
  */
 package Business.Organization;
 
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author aniketmirajkar
  */
-public class VerificationOrganization {
+public class VerificationOrganization extends Organization {
+    String name;
+    
+    public VerificationOrganization (String name) {
+        super(name);
+        this.name=name;
+    }
+    
+     @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new CriminalCheckRole());
+        return roles;
+    }
+    
+    @Override
+    public Type getType() {
+        return  Organization.Type.CriminalCheck;
+    }
     
 }
