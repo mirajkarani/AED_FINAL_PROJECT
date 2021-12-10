3f98a8bcef1ae74efd558866350ddd4dcffd57e2
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-09 21:41:43 -0500

    Added code for pharmacy organization

--- Final/src/Business/Organization/PharmacyOrganization.java
+++ Final/src/Business/Organization/PharmacyOrganization.java
@@ -4,10 +4,30 @@
  */
 package Business.Organization;
 
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author aniketmirajkar
  */
-public class PharmacyOrganization {
+public class PharmacyOrganization extends Organization {
     
+    String name;
+    
+    public PharmacyOrganization(String name) {
+        super(name);
+        this.name=name;
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new PharmacistRole());
+        return roles;
+    }
+    @Override
+    public Type getType() {
+        return  Organization.Type.Pharmacy;
+    }
 }
