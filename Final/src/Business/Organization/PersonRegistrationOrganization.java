2f7349dc8fe20758cb13f30f11f1e2dc0d475d0b
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-09 20:49:01 -0500

    Added missing class in organization

---
+++ Final/src/Business/Organization/PersonRegistrationOrganization.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Organization;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aniketmirajkar
+ */
+public class PersonRegistrationOrganization extends Organization {
+    
+    PersonDirectory personDirectory;
+    String name;
+
+    public PersonRegistrationOrganization(String name) {
+        super(name);
+        this.name=name;
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new PersonRegistrationRole());
+        return roles;
+    }
+    
+    @Override
+    public Organization.Type getType() {
+        return  Organization.Type.PersonRegistration;
+    }
+    
+}
