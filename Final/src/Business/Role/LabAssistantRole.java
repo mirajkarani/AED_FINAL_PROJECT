bc521b08cd4fe9352e097fafc84c0676e2742c8d
Deshmukh_Mandar_002194579 <deshmukh.ma@northeastern.edu>
2021-12-09 22:32:32 -0500

    Added LabAssistant and Modified Pharmacist Role

---
+++ Final/src/Business/Role/LabAssistantRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author mandardeshmukh
+ */
+public class LabAssistantRole {
+    
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, PersonDirectory Persondirectory, AdopterDirectory adopterdirectory, DonorDirectory donorDirectory) {
+        return new LabWorkArea(userProcessContainer, account, organization, business);
+    }
+    
+    @Override
+    public String toString(){
+        return (Role.RoleType.LabAssistant.getValue());
+    }
+}
---
+++ Final/src/Business/Role/PharmacistRole.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author mandardeshmukh
+ */
+public class PharmacistRole {
+    
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business, PersonDirectory Persondirectory, AdopterDirectory adopterdirectory,DonorDirectory donorDirectory) {
+       return new PharmacyWorkArea(userProcessContainer, account, (PharmacistOrganization)organization,enterprise, business,Persondirectory);
+    }
+    
+    @Override
+    public String toString(){
+        return (Role.RoleType.Pharmacist.getValue());
+    }
+}
