38051b1f2a9db45343b4eb626b365a25f793185a
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-11 02:40:09 -0500

    Added medical help jpanel classes

--- Final/src/userinterface/RayOfHope/PersonCare/RequestMedicalHelp.java
+++ Final/src/userinterface/RayOfHope/PersonCare/RequestMedicalHelp.java
@@ -7,11 +7,16 @@ package userinterface.RayOfHope.PersonCare;
 import Business.EcoSystem;
 import Business.Enterprise.Enterprise;
 import Business.Network.Network;
+import Business.Organization.MedicOrganization;
 import Business.Organization.Organization;
 import Business.Organization.PersonCareOrganization;
 import Business.Person.Person;
 import Business.Person.PersonDirectory;
 import Business.UserAccount.UserAccount;
+import Business.WorkQueue.MedicalAssistanceWorkRequest;
+import java.awt.CardLayout;
+import java.util.Date;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -43,6 +48,14 @@ public class RequestMedicalHelp extends javax.swing.JPanel {
         this.enterprise = enterprise;
         this.business = business;
         this.personDirectory = personDirectory;
+        
+        for (Network net : business.getNetworkCatalog()) {
+            for (Enterprise ent : net.getEnterpriseDirectory().getEnterpriseList()) {
+                if (ent.equals(enterprise)) {
+                    network = net;
+                }
+            }
+        }
     }
 
     /**
@@ -54,19 +67,110 @@ public class RequestMedicalHelp extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtSymptoms = new javax.swing.JTextArea();
+        btnSubmit = new javax.swing.JButton();
+        lblHeading = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel1.setText("Symptoms");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 260, 87, 42));
+
+        txtSymptoms.setColumns(20);
+        txtSymptoms.setRows(5);
+        jScrollPane1.setViewportView(txtSymptoms);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 210, 310, 110));
+
+        btnSubmit.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        btnSubmit.setText("Submit");
+        btnSubmit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSubmitActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnSubmit, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 400, 133, 33));
+
+        lblHeading.setFont(new java.awt.Font("SansSerif", 1, 20)); // NOI18N
+        lblHeading.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblHeading.setText("REQUEST MEDICAL HELP");
+        jPanel1.add(lblHeading, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 50, 460, 30));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 170, 450, 550));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 947, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 947, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 769, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 769, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnSubmitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSubmitActionPerformed
+        if (txtSymptoms.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please enter the symptoms");
+            return;
+        }
+        String symptoms = txtSymptoms.getText();
+        person.setMedicalStatus(person.getMedicalStatus() + "\n" + symptoms + " on date " + new Date());
+        person.setMedicalHelp(true);
+        MedicalAssistanceWorkRequest docwrkreq = new MedicalAssistanceWorkRequest();
+        docwrkreq.setStatus("Medically Unfit");
+        docwrkreq.setMessage(symptoms);
+        docwrkreq.setSender(account);
+        docwrkreq.setPersonId(person.getPersonId());
+        docwrkreq.setRemark("Request for Doctor");
+        docwrkreq.setPersonName(person.getName());
+        Organization org = null;
+        for (Network network : business.getNetworkCatalog()) {
+            for (Enterprise ent : network.getEnterpriseDirectory().getEnterpriseList()) {
+                for (Organization organization : ent.getOrganizationDirectory().getOrganizationList()) {
+                    if (organization instanceof MedicOrganization) {
+                        org = organization;
+                        break;
+                    }
+                }
+            }
+        }
+        if (org != null) {
+            org.getWorkQueue().getWorkRequestList().add(docwrkreq);
+            account.getWorkQueue().getWorkRequestList().add(docwrkreq);
+            business.getWorkQueue().getWorkRequestList().add(docwrkreq);
+        }
+        ViewCompletePersonDetails vccd = new ViewCompletePersonDetails(userProcessContainer, account, personCareOrganization, enterprise, business, personDirectory, person, true);
+        this.userProcessContainer.add("ViewCompleteChildDetails", vccd);
+        CardLayout layout = (CardLayout) userProcessContainer.getLayout();
+        layout.next(userProcessContainer);
+    }//GEN-LAST:event_btnSubmitActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSubmit;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblHeading;
+    private javax.swing.JTextArea txtSymptoms;
     // End of variables declaration//GEN-END:variables
 }
