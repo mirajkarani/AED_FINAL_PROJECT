c43779a947663da2babb109ee936582e39f1fc52
Mirajkar_Aniket_001563245 <mirajkar.a@northeastern.edu>
2021-12-11 19:28:59 -0500

    Adding binary file in gitignore

--- Final/src/userinterface/SystemAdminWorkArea/Services.java
+++ Final/src/userinterface/SystemAdminWorkArea/Services.java
@@ -4,6 +4,23 @@
  */
 package userinterface.SystemAdminWorkArea;
 
+import Business.EcoSystem;
+import Business.WorkQueue.AdoptionProcessWorkRequest;
+import Business.WorkQueue.EducationAssistWorkRequest;
+import Business.WorkQueue.MedicalAssistanceWorkRequest;
+import Business.WorkQueue.PersonCareWorkRequest;
+import Business.WorkQueue.WorkRequest;
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
 /**
  *
  * @author aniketmirajkar
@@ -13,8 +30,60 @@ public class Services extends javax.swing.JPanel {
     /**
      * Creates new form Services
      */
-    public Services() {
+    
+    JPanel userProcessContainer;
+    EcoSystem system;
+    JFreeChart barChart;
+    
+    public Services(JPanel userProcessContainer, EcoSystem system) {
         initComponents();
+        
+        this.userProcessContainer = userProcessContainer;
+        this.system = system;
+        populateBarGraph();
+    }
+    
+    public void populateBarGraph() {
+        ArrayList<AdoptionProcessWorkRequest> adpReqList = new ArrayList<>();
+        ArrayList<MedicalAssistanceWorkRequest> medReqList = new ArrayList<>();
+        ArrayList<EducationAssistWorkRequest> finReqList = new ArrayList<>();
+        ArrayList<PersonCareWorkRequest> ccReqList = new ArrayList<>();
+        Map<String, Integer> workReqMap = new HashMap<>();
+        for (WorkRequest workQue : system.getWorkQueue().getWorkRequestList()) {
+            if (workQue instanceof AdoptionProcessWorkRequest) {
+                adpReqList.add((AdoptionProcessWorkRequest) workQue);
+            } else if (workQue instanceof MedicalAssistanceWorkRequest) {
+                medReqList.add((MedicalAssistanceWorkRequest) workQue);
+            } else if (workQue instanceof EducationAssistWorkRequest) {
+                finReqList.add((EducationAssistWorkRequest) workQue);
+            }
+            else if (workQue instanceof PersonCareWorkRequest) {
+                ccReqList.add((PersonCareWorkRequest) workQue);
+        }
+
+        }
+        workReqMap.put("Adoption Services", adpReqList.size());
+        workReqMap.put("Medical Help Services", medReqList.size());
+        workReqMap.put("Financial Help Services", finReqList.size());
+        workReqMap.put("Care Co-ordination Services", ccReqList.size());
+
+        barChart = ChartFactory.createPieChart(
+                "Services At a Glance",
+                createDataset(workReqMap),
+                true, true, false);
+
+        ChartPanel chartPanel = new ChartPanel(barChart);
+        jPanel1.removeAll();
+        jPanel1.add(chartPanel, BorderLayout.CENTER);
+        jPanel1.validate();
+    }
+    
+    private PieDataset createDataset(Map<String, Integer> workReqMap) {
+        final DefaultPieDataset dataset = new DefaultPieDataset();
+        for (String r : workReqMap.keySet()) {
+            dataset.setValue(r, workReqMap.get(r));
+        }
+        return dataset;
     }
 
     /**
